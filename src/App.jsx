9b393df35a8e6e671a842a5fac285921d8b94e89
Angl99 <anggelplasencia@pursuit.org>
2024-07-16 16:34:07 -0400

    try4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Intro from './components/Intro';
-import Portfolio from './components/Portfolio';
-import Timeline from './components/Timeline';
+import Contact from './components/Contact.jsx';
+import Footer from './components/Footer.jsx';
+import Intro from './components/Intro.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Timeline from './components/Timeline.jsx';.jsx
 
 function App() {
 	const [theme, setTheme] = useState(null);

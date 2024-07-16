f6e5d65ea02a72bbfbe8207d9779d6c279d36b16
Angl99 <anggelplasencia@pursuit.org>
2024-07-16 16:29:23 -0400

    try 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://angl99.github.io/portfolio"
+  assetsInclude: ["**/*.JPG"],
+  base: "./",
 })

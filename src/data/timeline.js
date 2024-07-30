7d61a7c9b2a1ea9ae72f77b3d01245508e3eff05
Angl99 <anggelplasencia@pursuit.org>
2024-07-30 17:23:32 -0400

    changes

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -6,7 +6,7 @@ export default [
         details:'Joined the pursuit program to move myself to the next level of my career and further my passion for software engineering.'
     },
     {
-        year: '2021',
+        year: '2022',
         title: "Bachelor's Degree in Information Technology",
         duration: '4 years',
         details:'Started my career in Tech and introduced me to the world of software through some computer science classes.'
